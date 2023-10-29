d1775eeafeaf78c714a8889f3c49fd601c4452b4
Nishith Savla <nishithsavla005@gmail.com>
2023-10-29 22:44:15 +0530

    fix: Update social media handles

--- src/app/page.js
+++ src/app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
           </div>
 
           <div className="links mob:py-3 flex justify-center items-center space-x-5">
-            <Link href="https://chat.whatsapp.com/J1EjBNlkKRd5ONx4eQ57br">
+            <Link href="https://chat.whatsapp.com/JExDg8OH0lh1dIM044JKHm">
               <div className="cursor-pointer medium">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +77,7 @@ export default function Home() {
                 </svg>
               </div>
             </Link>
-            <Link href="https://www.linkedin.com/in/gdsc-poornima-university-34696528a/">
+            <Link href="https://www.linkedin.com/company/gdsc-vjti/">
               <div className="cursor-pointer linkedin">
                 <svg
                   className="w-6"
@@ -91,7 +91,7 @@ export default function Home() {
                 </svg>
               </div>
             </Link>
-            <Link href="https://www.instagram.com/gdsc.poornima">
+            <Link href="https://www.instagram.com/gdsc_vjti">
               <div className="cursor-pointer insta">
                 <svg
                   className="w-6"
@@ -105,7 +105,7 @@ export default function Home() {
                 </svg>
               </div>
             </Link>
-            <Link href="https://twitter.com/gdsc_poornima/">
+            <Link href="https://twitter.com/gdsc_vjti/">
               <div className="cursor-pointer twitter">
                 <svg
                   className="w-6"
